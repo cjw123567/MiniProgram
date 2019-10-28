997a46b181b679701aabde193eeabfe43eae29d5
[cjw123567] <624101826@qq.com>
2019-10-28 19:53:41 +0800

    0101_第一天学习

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,27 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name:"cjw",
+    students:[
+      {id:'111',name:'cjw',age:18},
+      {id: '112', name: 'cja', age: 21 },
+      {id: '113', name: 'cjc', age: 23 },
+      {id: '114', name: 'cjd', age: 22 }
+    ],
+    counter:0
   },
+  handleBtnClick(){
+    // 这是vue写法，不是微信小程序写法
+    //this.data.counter+=1
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    //正确写法
+    this.setData({
+      counter:this.data.counter+1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleBtnSubClick(){
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
